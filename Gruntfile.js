44ed5c53ff4454d73338148c3724cbda42d1e0b8
Brian Crowder <crowder@fiverocks.com>
2015-08-09 12:41:15 -0700

    Packaging and organizational fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,14 @@
 module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-screeps');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-flow');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     grunt.initConfig({
+        clean: {
+            dist: [ 'build' ]
+        },
         screeps: {
             options: {
                 email: 'krelin@fiverocks.com',
@@ -10,8 +16,27 @@ module.exports = function(grunt) {
                 branch: 'default'
             },
             dist: {
-                src: ['*.js']
+                src: ['build/*.js']
             }
-        }
+        },
+        flow: {
+            files: {}
+        },
+        browserify: {
+            dist: {
+                src: [ 'lib/main.js' ],
+                dest: 'build/main.js'
+            },
+            options: {
+                browserifyOptions: {
+                    debug: true
+                },
+                plugin: [
+                    [ 'minifyify' ]
+                ]
+            }
+        },
     });
+
+    grunt.registerTask('default', ['clean', 'browserify', 'screeps']);
 }
